6529ed9dfdf53a0cf3fd54c21d5b7d6fd0f08d52
mshaileshr@gmail.com <Yes@383840>
2020-08-18 12:43:56 +0530

    products and news added

--- src/main/java/com/contentstack/spring/example/ExampleApplication.java
+++ src/main/java/com/contentstack/spring/example/ExampleApplication.java
@@ -1,34 +1,85 @@
 package com.contentstack.spring.example;
+
 import com.contentstack.sdk.Error;
 import com.contentstack.sdk.*;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
 public class ExampleApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
-        Stack stack = Contentstack.stack("blt02f7b45378b008ee",
-                "cs5b69faf35efdebd91d08bcf4", "production");
-        Query query = stack.contentType("product").query();
-        query.includeContentType();
-        query.find(new QueryResultsCallBack() {
-            @Override
-            public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
-                if (error == null) {
-                    List<Entry> entryList = queryresult.getResultObjects();
-                    if (queryresult.getResultObjects().size() > 0) {
-                        for (Entry entry : entryList) {
-                            System.out.println("entry: " + entry.getTitle());
+
+        // Main call from where app starts to run.
+
+        System.out.println("News Headlines: "+new ExampleApplication().getNewsHeadlines());
+        System.out.println("News Products: "+new ExampleApplication().getProductList());
+    }
+
+
+    private ArrayList<NewsModel> getNewsHeadlines() {
+
+        try {
+            ArrayList<NewsModel> newsHeadlines = new ArrayList<>();
+            final Stack stack = Contentstack.stack("blt7979d15c28261b93", "cs17465ae5683299db9d259cb6", "production");
+            ContentType contentType = stack.contentType("news");
+            Query query = contentType.query();
+            query.find(new QueryResultsCallBack() {
+                @Override
+                public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
+                    if (error == null) {
+                        List<Entry> result = queryresult.getResultObjects();
+                        for (Entry entry : result) {
+                            JSONObject response = entry.toJSON();
+                            newsHeadlines.add(new NewsModel(response.optString("title"), response.optString("body"), ""));
                         }
                     }
-                } else {
-                    System.out.println("Error: " + error);
                 }
-            }
-        });
+            });
+            return newsHeadlines;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    private ArrayList<String> getProductList() {
+
+
+        try {
+
+            ArrayList<String> allProducts = new ArrayList<>();
+            Stack stack = Contentstack.stack("blt02f7b45378b008ee", "cs5b69faf35efdebd91d08bcf4", "production");
+            Query query = stack.contentType("product").query();
+            query.includeContentType();
+            query.find(new QueryResultsCallBack() {
+                @Override
+                public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
+                    if (error == null) {
+                        List<Entry> entryList = queryresult.getResultObjects();
+                        if (queryresult.getResultObjects().size() > 0) {
+                            for (Entry entry : entryList) {
+                                JSONObject response = entry.toJSON();
+                                allProducts.add(response.optString("title"));
+                            }
+                        }
+                    }
+                }
+            });
+            return allProducts;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
---
+++ src/main/java/com/contentstack/spring/example/NewsModel.java
@@ -0,0 +1,47 @@
+package com.contentstack.spring.example;
+
+public class NewsModel {
+
+    private String title;
+    private String description;
+    private String image;
+
+    public NewsModel(String title, String description, String image) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "NewsModel{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
