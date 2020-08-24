e37163d064bf947c5330dcf0ef3791c26ab4f2ed
Rishiraj Upadhyay <rishiraj.upadhyay@gmail.com>
2020-08-24 16:07:43 +0530

    Created new HTML page UI

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $('.dmtop').css({ bottom: "25px" });
+        } else {
+            $('.dmtop').css({ bottom: "-100px" });
+        }
+    });
+    $('.dmtop').click(function () {
+        $('html, body').animate({ scrollTop: '0px' }, 800);
+        return false;
+    });
+
+    // var divs = window.document.querySelectorAll('div.content');
+    // divs.forEach(function (div) {
+    //     div.innerHTML = div.innerText;
+    // });
+
+});
