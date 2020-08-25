5944045ab74a8f24d1c50f9cebf70ecd2a4a6d12
mshaileshr@gmail.com <Yes@383840>
2020-08-25 19:31:56 +0530

    function completed

--- src/main/java/com/contentstack/webapp/NewsModel.java
+++ src/main/java/com/contentstack/webapp/NewsModel.java
@@ -1,47 +1,49 @@
 package com.contentstack.webapp;
 
 public class NewsModel {
+	
+	private String title;
+	private String description;
+	private String image;
 
-    private String title;
-    private String description;
-    private String image;
 
-    public NewsModel(String title, String description, String image) {
-        this.title = title;
-        this.description = description;
-        this.image = image;
-    }
+	public NewsModel() {
 
-    @Override
-    public String toString() {
-        return "NewsModel{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", image='" + image + '\'' +
-                '}';
-    }
+	}
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	
+	public NewsModel(String title, String description, String imageURL) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.image = imageURL;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
 
-    public String getImage() {
-        return image;
-    }
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
+	
 
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
